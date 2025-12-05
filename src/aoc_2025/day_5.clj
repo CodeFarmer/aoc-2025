4d7859ac36ce2500be8201dee11868a0084c34ad
Joel <joel@gluth.io>
2025-12-05 12:34:09 +0000

    Empty source files for day 5 later on

---
+++ src/aoc_2025/day_5.clj
@@ -0,0 +1,4 @@
+(ns aoc-2025.day-5
+  (:require [aoc-2025.core :refer :all]
+            [clojure.string :as str]))
+
---
+++ test/aoc_2025/day_5_test.clj
@@ -0,0 +1,6 @@
+(ns aoc-2025.day-5-test
+  (:require [clojure.test :refer :all]
+            [aoc-2025.day-5 :refer :all]
+            [aoc-2025.core :refer :all]
+            [clojure.string :as str]))
+

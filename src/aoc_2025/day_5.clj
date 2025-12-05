1c9fd007b41ae2a79d90bf1c70f69bcaed58a575
Joel <joel@gluth.io>
2025-12-05 18:18:24 +0000

    Day 5 part 2

--- src/aoc_2025/day_5.clj
+++ src/aoc_2025/day_5.clj
@@ -13,3 +13,41 @@
         (if (<= a i b) true
             (recur (rest ranges) i)))))
 
+;; part 2
+
+
+(defn overlapping? [[a1 a2] [b1 b2]]
+  (or (<= b1 a1 b2)
+      (<= b1 a2 b2)
+      (<= a1 b1 a2)
+      (<= a1 b2 a2)))
+
+(defn merge-ranges [[a1 b1] [a2 b2]]
+  [(min a1 a2) (max b1 b2)])
+
+;; merge the range into the first overlapping range, otherwise cons it on the front
+(defn merge-into
+  ([aseq r]
+   (merge-into aseq r []))
+  ([tseq r hseq]
+   (if (empty? tseq)
+     (cons r hseq)
+     (let [h (first tseq)]
+       (if (overlapping? r h)
+         (concat hseq (cons (merge-ranges r h) (rest tseq)))
+         (recur (rest tseq) r (conj hseq h)))))))
+
+(defn count-ranges [aseq]
+  (reduce + (map (fn [[a b]] (inc (- b a))) aseq)))
+
+(defn merge-all-ranges
+  ([aseq]
+   (merge-all-ranges [] aseq))
+  ([hseq tseq]
+   (if (empty? tseq)
+     hseq
+     (let [[r & rest] tseq
+           merged (merge-into rest r)]
+       (if (= (count tseq) (count merged)) ;; no merge
+         (recur (conj hseq r) rest)
+         (recur hseq merged))))))
--- test/aoc_2025/day_5_test.clj
+++ test/aoc_2025/day_5_test.clj
@@ -45,3 +45,50 @@
   (let [[ranges items] input-data
         fresh'? (partial fresh? ranges)]
     (is (= 640 (count (filter fresh'? items))))))
+
+;; part 2
+
+(deftest overlapping-test
+  (is (not (overlapping? [3 5] [10 14])))
+  (is (overlapping? [10 14] [12 18]))
+  (is (overlapping? [16 20] [12 18]))
+  (is (overlapping? [16 17] [12 18]))
+  (is (overlapping? [12 18] [16 17])))
+
+(deftest merge-ranges-test
+  (is (= [10 18] (merge-ranges [10 14] [12 18])))
+  (is (= [12 20] (merge-ranges [16 20] [12 18])))
+  (is (= [12 18] (merge-ranges [16 17] [12 18]))))
+
+(deftest merge-into-test
+  (is (= [[ 3  5]
+          [10 14]
+          [16 20]
+          [12 18]]
+         (merge-into [[10 14]
+                      [16 20]
+                      [12 18]]
+                     [3 5])))
+  (is (= [[3 5] [16 20] [10 18]]
+         (merge-into [[3 5]
+                      [16 20]
+                      [12 18]]
+                     [10 14])))
+  (is (= [[3 5] [7 9]]
+         (merge-into [[3 5] [7 9]] [3 5]))))
+
+(deftest counting-ranges-test
+  (is (= 14 (count-ranges [[3 5] [10 20]]))))
+
+(deftest merging-all-ranges-test
+  (is (= [[ 3  5]
+          [10 20]]
+         (merge-all-ranges [[ 3  5]
+                            [10 14]
+                            [16 20]
+                            [12 18]])))
+  ;; this is a bug
+  (is (= [[3 5] [7 9]] (merge-all-ranges [[3 5] [3 5] [7 9]]))))
+
+(deftest part-2-test
+  (is (= 365804144481581 (count-ranges (merge-all-ranges (first input-data))))))

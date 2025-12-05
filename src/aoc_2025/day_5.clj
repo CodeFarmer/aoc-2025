b571edde2daf83186a7bf6594b0af6b13b7a8da3
Joel <joel@gluth.io>
2025-12-05 16:26:17 +0000

    Day 5 part 1

--- src/aoc_2025/day_5.clj
+++ src/aoc_2025/day_5.clj
@@ -2,3 +2,14 @@
   (:require [aoc-2025.core :refer :all]
             [clojure.string :as str]))
 
+(defn parse [astr]
+  (let [[first-part second-part] (str/split astr #"\n\n")]
+    [(map #(map parse-long (str/split % #"-")) (str/split first-part #"\n"))
+     (map parse-long (str/split second-part #"\n"))]))
+
+(defn fresh? [ranges i]
+  (if (empty? ranges) false
+      (let [[a b] (first ranges)]
+        (if (<= a i b) true
+            (recur (rest ranges) i)))))
+
--- test/aoc_2025/day_5_test.clj
+++ test/aoc_2025/day_5_test.clj
@@ -4,3 +4,44 @@
             [aoc-2025.core :refer :all]
             [clojure.string :as str]))
 
+(def sample-input
+  "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32")
+
+(deftest parsing-test
+  (is (= [[[ 3  5]
+           [10 14]
+           [16 20]
+           [12 18]]
+          [1 5 8 11 17 32]]
+         (parse sample-input))))
+
+;; part 1
+
+(deftest fresh-test
+  (let [[ranges items] (parse sample-input)
+        fresh'? (partial fresh? ranges)]
+    (is (not (fresh'? 1)))
+    (is (fresh'? 5))
+    (is (not (fresh'? 8)))
+    (is (fresh'? 11))
+    (is (fresh'? 17))
+    (is (not (fresh'? 32)))
+
+    (is (= 3 (count (filter fresh'? items))))))
+
+(def input-data (parse (slurp "aoc-2025-inputs/day-5.txt")))
+
+(deftest part-1-test
+  (let [[ranges items] input-data
+        fresh'? (partial fresh? ranges)]
+    (is (= 640 (count (filter fresh'? items))))))

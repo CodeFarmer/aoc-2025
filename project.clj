6988622d2e444a93e5de9ec0af979d736caec987
Joel <joel@gluth.io>
2025-12-11 18:29:18 +0000

    Added some visualisation preparations

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.2"]
-                 [org.clojure/math.combinatorics "0.3.0"]]
+                 [org.clojure/math.combinatorics "0.3.0"]
+                 [quil "4.3.1563"]]
   :repl-options {:init-ns aoc-2025.core})
--- src/aoc_2025/day_9.clj
+++ src/aoc_2025/day_9.clj
@@ -12,3 +12,15 @@
 
 (defn max-area [points]
   (reduce max (map #(apply area %) (c/combinations points 2))))
+
+;; visualisation
+
+(defn get-limits [points]
+  (reduce (fn [[[minx miny] [maxx maxy]] [px py]]
+            [[(min minx px) (min miny py)] [(max maxx px) (max maxy py)]])
+          [[Integer/MAX_VALUE Integer/MAX_VALUE]
+           [Integer/MIN_VALUE Integer/MIN_VALUE]]
+          points))
+
+  ;; part 2
+  
